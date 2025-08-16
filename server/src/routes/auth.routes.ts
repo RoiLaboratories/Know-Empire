cf915df6f1ae3332bb045dcf572bbfe410fbcd07
RoiLaboratories <roilaboratories@gmail.com>
2025-08-16 18:14:38 +0100

    refactor: move database migrations to db/ directory and remove unused server

--- server/src/routes/auth.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import * as authController from '../controllers/auth.controller';
-
-const router = Router();
-
-router.post('/verify', authController.verifyAuth);
-
-export default router;
