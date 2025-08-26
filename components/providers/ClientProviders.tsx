7884e5b81104f53f14065f700513ed74a266155c
RoiLaboratories <roilaboratories@gmail.com>
2025-08-26 07:54:45 +0100

    app updated

--- components/providers/ClientProviders.tsx
+++ components/providers/ClientProviders.tsx
@@ -5,18 +5,22 @@ import OrdersProvider from "@/providers/orders";
 import { Toaster } from 'react-hot-toast';
 import { WagmiProviderWrapper } from "@/providers/wagmi";
 import { FarcasterAuthProvider } from "@/context/FarcasterAuthContext";
+import { MiniKitProvider } from '@coinbase/onchainkit/minikit';
+import { base } from 'wagmi/chains';
 
 const ClientProviders = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <WagmiProviderWrapper>
-        <FarcasterAuthProvider>
-          <CartProvider>
-            <OrdersProvider>
-              {children}
-            </OrdersProvider>
-          </CartProvider>
-        </FarcasterAuthProvider>
+        <MiniKitProvider chain={base}>
+          <FarcasterAuthProvider>
+            <CartProvider>
+              <OrdersProvider>
+                {children}
+              </OrdersProvider>
+            </CartProvider>
+          </FarcasterAuthProvider>
+        </MiniKitProvider>
       </WagmiProviderWrapper>
       <Toaster 
         toastOptions={{
