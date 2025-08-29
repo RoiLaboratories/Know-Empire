dbdfba5d58143d3f3c7030c5a55c7653bb7da85f
RoiLaboratories <roilaboratories@gmail.com>
2025-08-29 04:34:48 +0100

    app updated

--- app/marketplace/buy/layout.tsx
+++ app/marketplace/buy/layout.tsx
@@ -1,12 +1,13 @@
-import Header from "../../components/layout/Header";
+"use client";
+import Header from "../../../components/layout/Header";
 
-function MaketPlacelayout({ children }: { children: React.ReactNode }) {
+function BuyFormLayout({ children }: { children: React.ReactNode }) {
   return (
     <section className="flex flex-col items-center min-h-screen pt-3 px-3">
       <div className="w-full max-w-lg flex flex-col flex-1">
         <Header />
 
-        {/*market place */}
+        {/*form container */}
         <div className="bg-white rounded-t-xl pt-5 flex flex-col gap-y-3 pb-3 px-3">
           {children}
         </div>
@@ -15,4 +16,4 @@ function MaketPlacelayout({ children }: { children: React.ReactNode }) {
   );
 }
 
-export default MaketPlacelayout;
+export default BuyFormLayout;
