a55b53a853974b6d94dcf52912b9063704cf7f4c
RoiLaboratories <roilaboratories@gmail.com>
2025-08-29 04:55:00 +0100

    app updated

--- app/marketplace/buy/layout.tsx
+++ app/marketplace/buy/layout.tsx
@@ -1,19 +1,11 @@
-// "use client";
-// import Header from "../../../components/layout/Header";
+"use client";
 
-// function BuyFormLayout({ children }: { children: React.ReactNode }) {
-//   return (
-//     <section className="flex flex-col items-center min-h-screen pt-3 px-3">
-//       <div className="w-full max-w-lg flex flex-col flex-1">
-//         <Header />
+function BuyFormLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="bg-white rounded-t-xl pt-5 flex flex-col gap-y-3 pb-3 px-3">
+      {children}
+    </div>
+  );
+}
 
-//         {/*form container */}
-//         <div className="bg-white rounded-t-xl pt-5 flex flex-col gap-y-3 pb-3 px-3">
-//           {children}
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
-
-// export default BuyFormLayout;
+export default BuyFormLayout;
